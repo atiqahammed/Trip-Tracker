bf13be3873f81d3f05e9f9ecf67cf3b7ef268e52
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-18 18:29:59 +0600

    trip object has been updated with property

--- source/TripTickerLive/TripTicker.BackService/Models/Trip.cs
+++ source/TripTickerLive/TripTicker.BackService/Models/Trip.cs
@@ -7,5 +7,12 @@ namespace TripTicker.BackService.Models
 {
     public class Trip
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
     }
 }
